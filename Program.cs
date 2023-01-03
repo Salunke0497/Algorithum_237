8ae50c5b316fa57ae519ee576e250170c6cc66b2
Salunke0497 <113031652+Salunke0497@users.noreply.github.com>
2023-01-04 00:42:44 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BubbleSort bubbleSort = new BubbleSort();
+            bubbleSort.DisplayBubbleSort();
+            InsertionSort insertionSort = new InsertionSort();
+            //insertionSort.DisplayInsertionSort();
+            //Console.ReadLine();
+        }
+    }
+}
